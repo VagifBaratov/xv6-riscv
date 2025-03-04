0d2c2e95da9ea987419ce5c075d2060656492dca
Vagif <myspaceofmyheart@gmail.com>
2025-03-04 23:56:31 +0300

    lab01

---
+++ task3.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+    int pfd[2];
+    pid_t pid;
+
+    if (pipe(pfd) == -1) {
+        perror("Error: Failed to create pipe\n");
+        exit(1);
+    }
+
+    pid = fork();
+    if (pid == -1) {
+        perror("Error: Failed to fork\n");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        close(pfd[1]);
+
+        char buffer[128];
+        ssize_t bytes_read;
+        while ((bytes_read = read(pfd[0], buffer, sizeof(buffer))) > 0) {
+            write(1, buffer, bytes_read);
+        }
+
+        close(pfd[0]); 
+        exit(0);
+    } else {
+        close(pfd[0]);
+
+        for (int i = 1; i < argc; i++) {
+            if (write(pfd[1], argv[i], strlen(argv[i])) < 0) {
+                perror("Error: Failed write to pipe\n");
+                exit(1);
+            }
+            if (write(pfd[1], "\n", 1) < 0) {
+                perror("Error: Failed write to pipe\n");
+                exit(1);
+            }
+        }
+        
+        if (close(pfd[1]) < 0) {
+            perror( "Error: Failed to close write pipe\n");
+            exit(1);
+        }
+
+        if (wait(0) < 0) {
+            perror("Error: Failed to wait\n");
+            exit(1);
+        }
+        exit(0);
+    }
+}
